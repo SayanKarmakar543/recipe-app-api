128a61312edd6c3b4c082b3194bef11b6ca59cda
Sayan Karmakar <sayankarmakar543@gmail.com>
2025-04-24 00:49:08 +0530

    recreate proxy dir.

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
+
 set -e
 
-# Generate config with SSL settings
-envsubst '${LISTEN_PORT} ${APP_HOST} ${APP_PORT}' \
-  < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
-
-# Start Nginx
-exec nginx -g "daemon off;"
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
